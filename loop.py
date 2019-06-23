e4aa51c5d4944644e8bb19cbb60506818661300f
aifa21 <37891597+aifa21@users.noreply.github.com>
2019-06-23 16:42:33 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+    fruits = ["apple", "banana", "cherry"]
+    for x in fruits:
+        if x=="banana":
+            break;
+
+
+i=1
+while i<6:
+    print(i)
+    i+=1
